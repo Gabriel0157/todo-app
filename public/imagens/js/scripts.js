d1e3c091c2ea3d20b556ebdd0d07fdb0b02523c9
Gabriel Pereira Rodrigues <gp776945@gmail.com>
2023-11-30 08:05:26 -0300

    deixando a mudança de tema dinamica

---
+++ public/imagens/js/scripts.js
@@ -0,0 +1,46 @@
+function alterarTema() {
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+    if (tema) {
+        let novoTema
+
+        if (tema === "light") {
+            novoTema = "dark"
+            button.classList = `<img src="/imgagens/sun-icon.png" alt="icone de sol"`
+            body.classList.remove("light")
+            body.classList.add("dark")
+        } else {
+            novoTema = "light"
+            button.classList = `<img src="/imgagens/moon-icon.png" alt="icone de lua"`
+            body.classList.remove("dark")
+            body.classList.add("light")
+        }
+
+        localStorage.setItem("tema", novoTema)
+        return
+    }
+
+    localStorage.setItem("tema", "dark")
+    body.classList.add("dark")
+}
+
+function verificarTema() {
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+    if (tema) {
+        if (tema === "dark") {
+            body.classList.add("dark")
+            button.classList = `<img src="/imgagens/sun-icon.png" alt="icone de sol"`
+            
+        } else {
+            body.classList.add("light")
+            button.classList = `<img src="/imgagens/moon-icon.png" alt="icone de lua"`
+        }
+    }
+}
+
+verificarTema()
